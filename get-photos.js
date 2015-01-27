8a4f9292a6ac65472cb619bbd2614616cc739a71
morganherlocker <morgan.herlocker@gmail.com>
2015-01-26 19:21:03 -0500

    mushers.json

---
+++ get-photos.js
@@ -0,0 +1,26 @@
+var request = require('request');
+var cheerio = require('cheerio');
+var fs = require('fs');
+
+var mushers = require('./mushers.json')
+
+request('http://iditarod.com/race/2013/', function(err, res, html) {
+	var $ = cheerio.load(html);
+	var count = 0;
+	$('[class^="musher"]').each(function(i, element){
+		var name = element.children[0].data;
+		request(element.attribs.href, function(err, res, photoHtml){
+			var $ = cheerio.load(photoHtml);
+			$('[itemprop="photo"]').each(function(j, photoElement){
+				mushers.forEach(function(musher){
+					if(name === musher.Name){
+						musher.img = photoElement.attribs.src;
+					}
+				});
+				count++;
+				if(count === mushers.length) fs.writeFileSync('img.json', JSON.stringify(mushers, null, 2))
+			});
+		});
+    });
+});
+
