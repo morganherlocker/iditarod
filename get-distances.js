2b331d2bb32e1b0acafbdaa01bede991899d3f0f
morganherlocker <morgan.herlocker@gmail.com>
2015-01-29 01:47:37 -0500

    calculate leg distances

--- get-distances.js
+++ get-distances.js
@@ -2,8 +2,12 @@ var turf = require('turf');
 var fs = require('fs');
 
 var route = JSON.parse(fs.readFileSync('./route.geojson'));
-var pts = JSON.parse(fs.readFileSync('./legs.geojson'));
+var legs = JSON.parse(fs.readFileSync('./legs.geojson'));
 
 route.properties.distance = turf.lineDistance(route, 'miles');
 fs.writeFileSync('./route.geojson', JSON.stringify(route, null, 2));
 
+legs.features.forEach(function(leg) {
+	leg.properties.distance = turf.lineDistance(leg, 'miles');
+});
+fs.writeFileSync('./legs.geojson', JSON.stringify(legs, null, 2));
