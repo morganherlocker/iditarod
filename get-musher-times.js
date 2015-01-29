f31621dfcceb3e4ab5065413d35511dfbc7402a4
morganherlocker <morgan.herlocker@gmail.com>
2015-01-29 17:03:57 -0500

    compute leg times

--- get-musher-times.js
+++ get-musher-times.js
@@ -6,7 +6,7 @@ var pts = JSON.parse(fs.readFileSync('./checkpoints.geojson'));
 var checktimes = require('./checktimes.json');
 var mushers = require('./mushers.json')
 
-// combine waypoint data
+// join leg data to mushers
 var waypoints = turf.featurecollection([]);
 pts.features.forEach(function(pt){
     checktimes.forEach(function(leg){
@@ -33,4 +33,48 @@ pts.features.forEach(function(pt){
     });
 });
 
+// compute js datetimes and rests as seconds and leg times in seconds
+mushers.forEach(function(musher){
+    if(musher.times){
+        for(var i = 0; i < musher.times.length; i++) {
+            musher.times[i].timein = stringToDatetime(musher.times[i].timein);
+            musher.times[i].timeout = stringToDatetime(musher.times[i].timeout);
+            musher.times[i].rest = restToSeconds(musher.times[i].rest);
+        }
+    }
+});
+
+// compute leg times in seconds
+mushers.forEach(function(musher){
+    if(musher.times){
+        for(var i = 0; i < musher.times.length - 1; i++) {
+            musher.times[i].legtime = (musher.times[i+1].timein - musher.times[i].timeout)/1000;
+        }
+    }
+});
+
 fs.writeFileSync('./final/mushers.json', JSON.stringify(mushers));
+
+function stringToDatetime(time) {
+    if(time) {
+        var year = 2014;
+        var  month = time.split(' ')[0].split('/')[0];
+        var  day = time.split(' ')[0].split('/')[1];
+        var  hours = time.split(' ')[1].split(':')[0];
+        var  minutes = time.split(' ')[1].split(':')[1];
+        var datetime = new Date(2014, month, day, hours, minutes, 0, 0);
+        return datetime
+    } else {
+        return '';
+    }
+}
+
+function restToSeconds(rest) {
+    if(rest) {
+        var hours = rest.split(' ')[0].split('h')[0];
+        var minutes = rest.split(' ')[1].split('m')[0];
+        return (hours * 60 * 60) + (minutes * 60)
+    } else {
+        return '';
+    }
+}
